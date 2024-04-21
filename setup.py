c44c804d52f2288a055c5824b4ba87dd8a417c98
Chitra Joshi <chitra.joshi@viit.ac.in>
2024-04-21 23:57:17 +0530

    new changes 21-Apr

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Chitra Joshi',
+    author_email= 'chitra.joshi@viit.ac.in',
+    packages= find_packages(),
+    install_requires = []
+
+)
